98aebfcd245bc4ae752c725f44722367e95171c8
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-08-27 22:12:33 -0300

    Add: rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'', component: HomeComponent },
+  {path:'inicio', component: HomeComponent },
+  {path:'contato', component: ContatoComponent }
+
+];
